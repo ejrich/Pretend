1e6ef828e345386e14fa4ddf48f6eb4e4c6c6df8
Evan Richardson <ejrich44@gmail.com>
2020-07-27 17:12:17 -0500

    Game scaffolding

--- Game/Application.cs
+++ Game/Application.cs
@@ -1,14 +1,27 @@
 using Pretend;
+using Pretend.Layers;
 
 namespace Game
 {
     public class Application : IApplication
     {
-        
+        private readonly ILayerContainer _layerContainer;
+        private readonly GameLayer _gameLayer;
+
+        public Application(ILayerContainer layerContainer, GameLayer gameLayer)
+        {
+            _layerContainer = layerContainer;
+            _gameLayer = gameLayer;
+        }
+
+        public void Start()
+        {
+            _layerContainer.PushLayer(_gameLayer);
+        }
     }
 
     public class WindowAttributes : IWindowAttributesProvider
     {
-        public string Title => "Game";
+        public string Title => "Possible Game";
     }
 }
