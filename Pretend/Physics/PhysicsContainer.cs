6c54e9f95754a7ad49300844676ee50787d84d54
Evan Richardson <ejrich44@gmail.com>
2020-09-08 08:01:21 -0500

    Implementing the initial determine collision method

--- Pretend/Physics/PhysicsContainer.cs
+++ Pretend/Physics/PhysicsContainer.cs
@@ -1,4 +1,6 @@
-﻿using OpenToolkit.Mathematics;
+﻿using System;
+using System.Reflection.Emit;
+using OpenToolkit.Mathematics;
 using Pretend.ECS;
 
 namespace Pretend.Physics
@@ -40,6 +42,20 @@ namespace Pretend.Physics
             // Recalculate velocity
             var deltaV = Gravity * timeStep;
             physicsComponent.Velocity += deltaV;
+        }
+
+        private Collision DetermineCollision(IEntity a, IEntity b)
+        {
+            var ap = a.GetComponent<PositionComponent>();
+            var bp = b.GetComponent<PositionComponent>();
+            var aSize = a.GetComponent<SizeComponent>();
+            var bSize = b.GetComponent<SizeComponent>();
+            var dx = Math.Abs(bp.X - ap.X) - (aSize.Width + bSize.Width);
+            var dy = Math.Abs(bp.Y - ap.Y) - (aSize.Height + bSize.Height);
+
+            if ((dx == 0 && dy > 0) || (dy == 0 && dx > 0)) return Collision.Touching;
+
+            return dx < 0 && dy < 0 ? Collision.Colliding : Collision.Apart;
         }
     }
 }
