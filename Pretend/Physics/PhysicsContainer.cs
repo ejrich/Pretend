2979c2715432e2a0ddf36b652d205fd538f745d1
Evan Richardson <ejrich44@gmail.com>
2020-09-09 20:34:56 -0500

    Working on the collision algorithm

--- Pretend/Physics/PhysicsContainer.cs
+++ Pretend/Physics/PhysicsContainer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using OpenToolkit.Mathematics;
 using Pretend.ECS;
@@ -21,7 +22,23 @@ namespace Pretend.Physics
             var entities = entityContainer.GetEntitiesWithComponent<PhysicsComponent>();
             for (var i = 0; i < 4; i++)
             {
+                var newPositions = new Dictionary<IEntity, Vector3>();
                 foreach (var entity in entities)
+                {
+                    var physicsComponent = entity.GetComponent<PhysicsComponent>();
+
+                    var position = entity.GetComponent<PositionComponent>();
+                    if (physicsComponent.Fixed)
+                    {
+                        newPositions.Add(entity, new Vector3(position.X, position.Y, position.Z));
+                    }
+                    else
+                    {
+                        var newPosition = CalculatePosition(physicsComponent, position, dt);
+                        newPositions.Add(entity, newPosition);
+                    }
+                }
+                foreach (var (entity, position) in newPositions)
                 {
                     foreach (var other in entities.Where(_ => _ != entity))
                     {
@@ -29,35 +46,60 @@ namespace Pretend.Physics
                         Console.WriteLine(collision);
                         if (collision == Collision.Colliding)
                         {
-                            // Console.WriteLine("Hello");
+                            var otherPosition = newPositions[other];
+                            InterpolateCollision(entity, other, position, otherPosition);
                         }
                     }
-                    var physicsComponent = entity.GetComponent<PhysicsComponent>();
-                    if (physicsComponent.Fixed) continue;
 
                     var positionComponent = entity.GetComponent<PositionComponent>();
-
-                    CalculatePosition(physicsComponent, positionComponent, dt);
+                    positionComponent.X = position.X;
+                    positionComponent.Y = position.Y;
+                    positionComponent.Z = position.Z;
                 }
             }
         }
 
-        private void CalculatePosition(PhysicsComponent physicsComponent, PositionComponent position, float timeStep)
+        private void InterpolateCollision(IEntity entity, IEntity other, Vector3 position, Vector3 otherPosition)
+        {
+            var (dx, dy) = CalculateDistance(entity, other);
+            var eFixed = entity.GetComponent<PhysicsComponent>().Fixed;
+            var oFixed = other.GetComponent<PhysicsComponent>().Fixed;
+
+            if (eFixed || oFixed)
+            {
+                entity.GetComponent<PhysicsComponent>().Velocity = Vector3.Zero;
+                other.GetComponent<PhysicsComponent>().Velocity = Vector3.Zero;
+            }
+        }
+
+        private Vector3 CalculatePosition(PhysicsComponent physicsComponent, PositionComponent position, float timeStep)
         {
             // Calculate delta p
             var (x, y, z) = physicsComponent.Velocity * timeStep + 0.5f * Gravity * timeStep * timeStep;
 
             // Calculate next position
-            position.X += x;
-            position.Y += y;
-            position.Z += z;
+            var newPosition = new Vector3(position.X += x, position.Y += y, position.Z += z);
+            // position.X += x;
+            // position.Y += y;
+            // position.Z += z;
 
             // Recalculate velocity
             var deltaV = Gravity * timeStep;
             physicsComponent.Velocity += deltaV;
+
+            return newPosition;
         }
 
         private static Collision DetermineCollision(IEntity a, IEntity b)
+        {
+            var (dx, dy) = CalculateDistance(a, b);
+
+            if ((dx == 0 && dy > 0) || (dy == 0 && dx > 0)) return Collision.Touching;
+
+            return dx < 0 && dy < 0 ? Collision.Colliding : Collision.Apart;
+        }
+
+        private static (float dx, float dy) CalculateDistance(IEntity a, IEntity b)
         {
             var aPos = a.GetComponent<PositionComponent>();
             var bPos = b.GetComponent<PositionComponent>();
@@ -66,9 +108,7 @@ namespace Pretend.Physics
             var dx = Math.Abs(bPos.X - aPos.X) - (aSize.Width / 2 + bSize.Width / 2);
             var dy = Math.Abs(bPos.Y - aPos.Y) - (aSize.Height / 2 + bSize.Height / 2);
 
-            if ((dx == 0 && dy > 0) || (dy == 0 && dx > 0)) return Collision.Touching;
-
-            return dx < 0 && dy < 0 ? Collision.Colliding : Collision.Apart;
+            return (dx, dy);
         }
     }
 }
