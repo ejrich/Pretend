0a8ace92ae3d817ff3322fd1d0eac5a779311ebb
Evan Richardson <ejrich44@gmail.com>
2020-11-20 16:08:25 -0600

    Updating layer tests

--- Tests/Pretend.Tests/Layers/LayerContainerTests.cs
+++ Tests/Pretend.Tests/Layers/LayerContainerTests.cs
@@ -37,6 +37,19 @@ namespace Pretend.Tests.Layers
             _target.Update(0);
 
             Assert.IsTrue(layer.UpdateCalled);
+            Assert.IsTrue(layer.RenderCalled);
+        }
+
+        [TestMethod]
+        public void Update_OnlyUpdatesUnPausedLayers()
+        {
+            var layer = new TestLayer { Paused = true };
+            _target.PushLayer(layer);
+
+            _target.Update(0);
+
+            Assert.IsFalse(layer.UpdateCalled);
+            Assert.IsTrue(layer.RenderCalled);
         }
 
         [TestMethod]
@@ -54,7 +67,10 @@ namespace Pretend.Tests.Layers
         private class TestLayer : ILayer
         {
             public bool UpdateCalled { get; private set; }
+            public bool RenderCalled { get; private set; }
+            public bool Paused { get; set; }
             public void Update(float timeStep) => UpdateCalled = true;
+            public void Render() => RenderCalled = true;
             public void HandleEvent(IEvent evnt) {}
         }
 
