141540c5344e4d89c1bac11cab25a0deb7573d8d
Evan Richardson <ejrich44@gmail.com>
2020-11-20 10:43:36 -0600

    Abstracting TextComponent creation

--- Sandbox/SettingsLayer.cs
+++ Sandbox/SettingsLayer.cs
@@ -41,14 +41,7 @@ namespace Sandbox
                     else
                         _.Settings.WindowMode |= WindowMode.Fullscreen;
                 }, _ => _.Settings.WindowMode.HasFlag(WindowMode.Fullscreen), fullscreenButton));
-            _scene.AddComponent(fullscreenButton, new TextComponent
-            {
-                Text = "Fullscreen",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 30,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(fullscreenButton, CreateButtonText("Fullscreen"));
 
             var borderlessButton= _scene.CreateEntity();
             _scene.AddComponent(borderlessButton, new PositionComponent { X = 530, Y = 100 });
@@ -62,14 +55,7 @@ namespace Sandbox
                     else
                         _.Settings.WindowMode |= WindowMode.Borderless;
                 }, _ => _.Settings.WindowMode.HasFlag(WindowMode.Borderless), borderlessButton));
-            _scene.AddComponent(borderlessButton, new TextComponent
-            {
-                Text = "Borderless",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 30,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(borderlessButton, CreateButtonText("Borderless"));
 
             var vsyncButton= _scene.CreateEntity();
             _scene.AddComponent(vsyncButton, new PositionComponent { X = 530, Y = 50 });
@@ -77,14 +63,7 @@ namespace Sandbox
             _scene.AddComponent(vsyncButton, new ColorComponent());
             _scene.AddComponent(vsyncButton, new SettingsScript(_settingsManager,
                 _ => _.Settings.Vsync = !_.Settings.Vsync, _ => _.Settings.Vsync, vsyncButton));
-            _scene.AddComponent(vsyncButton, new TextComponent
-            {
-                Text = "Vsync",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 30,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(vsyncButton, CreateButtonText("Vsync"));
 
             var zeroFpsButton= _scene.CreateEntity();
             _scene.AddComponent(zeroFpsButton, new PositionComponent { X = 460 });
@@ -92,14 +71,7 @@ namespace Sandbox
             _scene.AddComponent(zeroFpsButton, new ColorComponent());
             _scene.AddComponent(zeroFpsButton, new SettingsScript(_settingsManager,
                 _ => _.Settings.MaxFps = 0, _ => _.Settings.MaxFps == 0, zeroFpsButton));
-            _scene.AddComponent(zeroFpsButton, new TextComponent
-            {
-                Text = "0",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 30,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(zeroFpsButton, CreateButtonText("0"));
 
             var sixtyFpsButton= _scene.CreateEntity();
             _scene.AddComponent(sixtyFpsButton, new PositionComponent { X = 530 });
@@ -107,14 +79,7 @@ namespace Sandbox
             _scene.AddComponent(sixtyFpsButton, new ColorComponent());
             _scene.AddComponent(sixtyFpsButton, new SettingsScript(_settingsManager,
                 _ => _.Settings.MaxFps = 60, _ => _.Settings.MaxFps == 60, sixtyFpsButton));
-            _scene.AddComponent(sixtyFpsButton, new TextComponent
-            {
-                Text = "60",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 30,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(sixtyFpsButton, CreateButtonText("60"));
 
             var highFpsButton= _scene.CreateEntity();
             _scene.AddComponent(highFpsButton, new PositionComponent { X = 600 });
@@ -122,14 +87,7 @@ namespace Sandbox
             _scene.AddComponent(highFpsButton, new ColorComponent());
             _scene.AddComponent(highFpsButton, new SettingsScript(_settingsManager,
                 _ => _.Settings.MaxFps = 144, _ => _.Settings.MaxFps == 144, highFpsButton));
-            _scene.AddComponent(highFpsButton, new TextComponent
-            {
-                Text = "144",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 30,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(highFpsButton, CreateButtonText("144"));
 
             var resolution1Button= _scene.CreateEntity();
             _scene.AddComponent(resolution1Button, new PositionComponent { X = 479, Y = -50 });
@@ -141,14 +99,7 @@ namespace Sandbox
                     _.Settings.ResolutionX = 1280;
                     _.Settings.ResolutionY = 720;
                 }, _ => _.Settings.ResolutionX == 1280 && _.Settings.ResolutionY == 720, resolution1Button));
-            _scene.AddComponent(resolution1Button, new TextComponent
-            {
-                Text = "1280x720",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 20,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(resolution1Button, CreateButtonText("1280x720", 20));
 
             var resolution2Button= _scene.CreateEntity();
             _scene.AddComponent(resolution2Button, new PositionComponent { X = 581, Y = -50 });
@@ -160,42 +111,21 @@ namespace Sandbox
                     _.Settings.ResolutionX = 1920;
                     _.Settings.ResolutionY = 1080;
                 }, _ => _.Settings.ResolutionX == 1920 && _.Settings.ResolutionY == 1080, resolution2Button));
-            _scene.AddComponent(resolution2Button, new TextComponent
-            {
-                Text = "1920x1080",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 20,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(resolution2Button, CreateButtonText("1920x1080", 20));
 
             var applyButton= _scene.CreateEntity();
             _scene.AddComponent(applyButton, new PositionComponent { X = 530, Y = -100 });
             _scene.AddComponent(applyButton, new SizeComponent { Width = 200, Height = 40 });
             _scene.AddComponent(applyButton, new ColorComponent());
             _scene.AddComponent(applyButton, new SettingsScript(_settingsManager, _ => _.Apply(), _ => false, applyButton));
-            _scene.AddComponent(applyButton, new TextComponent
-            {
-                Text = "Apply",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 30,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(applyButton, CreateButtonText("Apply"));
 
             var resetButton= _scene.CreateEntity();
             _scene.AddComponent(resetButton, new PositionComponent { X = 530, Y = -150 });
             _scene.AddComponent(resetButton, new SizeComponent { Width = 200, Height = 40 });
             _scene.AddComponent(resetButton, new ColorComponent());
             _scene.AddComponent(resetButton, new SettingsScript(_settingsManager, _ => _.Reset(), _ => false, resetButton));
-            _scene.AddComponent(resetButton, new TextComponent
-            {
-                Text = "Reset",
-                Font = "Assets/Roboto-Medium.ttf",
-                Size = 30,
-                RelativePosition = new Vector3(0, -2.5f, 0.01f),
-                Color = new Vector4(0, 0, 0, 1)
-            });
+            _scene.AddComponent(resetButton, CreateButtonText("Reset"));
         }
 
         public void Update(float timeStep)
@@ -222,6 +152,18 @@ namespace Sandbox
             if (!_visible) return;
 
             _scene.HandleEvent(evnt);
+        }
+
+        private static TextComponent CreateButtonText(string label, uint size = 30)
+        {
+            return new TextComponent
+            {
+                Text = label,
+                Font = "Assets/Roboto-Medium.ttf",
+                Size = size,
+                RelativePosition = new Vector3(0, -2.5f, 0.01f),
+                Color = new Vector4(0, 0, 0, 1)
+            };
         }
     }
 }
